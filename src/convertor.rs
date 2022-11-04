73b9cc2bd0208e2305e964a3afb604625968aab5
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-04 15:13:17 +0900

    add tosnake case camel and constant

--- src/convertor.rs
+++ src/convertor.rs
@@ -3,14 +3,33 @@ use crate::naming_principal::NamingPrincipal;
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct NamingPrincipalConvertor<'a> {
     original: &'a str,
-    value: NamingPrincipal<'a>,
+    principal: NamingPrincipal<'a>,
 }
 
 impl<'a> NamingPrincipalConvertor<'a> {
     pub fn new(source: &'a str) -> Self {
         Self {
             original: source,
-            value: NamingPrincipal::new(source),
+            principal: NamingPrincipal::new(source),
+        }
+    }
+    pub fn to_snake(&self) -> String {
+        match self.principal {
+            NamingPrincipal::Camel(camel) => camel.chars().fold(String::new(), |mut acc, cur| {
+                if cur.is_uppercase() {
+                    acc += "_";
+                    acc.push(cur.to_ascii_lowercase());
+                    acc
+                } else {
+                    acc.push(cur);
+                    acc
+                }
+            }),
+            NamingPrincipal::Constant(constant) => constant
+                .chars()
+                .map(|c| c.to_ascii_lowercase())
+                .collect::<String>(),
+            _ => self.original.to_string(),
         }
     }
 }
@@ -18,8 +37,31 @@ impl<'a> NamingPrincipalConvertor<'a> {
 #[cfg(test)]
 mod test_convertor {
     use super::*;
-    use crate::naming_principal::naming_principal_test_data;
+    use crate::naming_principal::naming_principal_test_data::*;
 
     #[test]
-    fn test_to_snake() {}
+    fn test_to_snake() {
+        let convertor = NamingPrincipalConvertor::new(FLATCASE);
+        assert_eq!(convertor.to_snake(), "flatcase".to_string());
+        let convertor = NamingPrincipalConvertor::new(EMPTYCASE);
+        assert_eq!(convertor.to_snake(), "".to_string());
+        let convertor = NamingPrincipalConvertor::new(SNAKE_CASE1);
+        assert_eq!(convertor.to_snake(), SNAKE_CASE1.to_string());
+        let convertor = NamingPrincipalConvertor::new(SNAKE_CASE2);
+        assert_eq!(convertor.to_snake(), SNAKE_CASE2.to_string());
+        let convertor = NamingPrincipalConvertor::new(CAMEL_CASE);
+        assert_eq!(convertor.to_snake(), "camel_case".to_string());
+        let convertor = NamingPrincipalConvertor::new(CONSTANT_CASE1);
+        assert_eq!(convertor.to_snake(), "constant_case".to_string());
+        let convertor = NamingPrincipalConvertor::new(CONSTANT_CASE2);
+        assert_eq!(convertor.to_snake(), "constant".to_string());
+        //let convertor = NamingPrincipalConvertor::new(PASCAL_CASE1);
+        //assert_eq!(convertor.to_snake(), "pascal_case".to_string());
+        //let convertor = NamingPrincipalConvertor::new(PASCAL_CASE2);
+        //assert_eq!(convertor.to_snake(), "a_b_c_data".to_string());
+        //let convertor = NamingPrincipalConvertor::new(CHAIN_CASE1);
+        //assert_eq!(convertor.to_snake(), "chain_case".to_string());
+        //let convertor = NamingPrincipalConvertor::new(CHAIN_CASE2);
+        //assert_eq!(convertor.to_snake(), "_chain_case".to_string());
+    }
 }
