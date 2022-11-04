5d3bd5f196b3e75a17dd8e8771f55535372fac95
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-04 14:13:59 +0900

    add chain

--- src/naming_principal.rs
+++ src/naming_principal.rs
@@ -37,7 +37,15 @@ impl<'a> NamingPrincipal<'a> {
         if Self::is_constant(source) {
             return Self::Constant(source);
         }
+        if Self::is_chain(source) {
+            return Self::Chain(source);
+        }
         Self::Flat(source)
+    }
+    pub fn is_chain(source: &'a str) -> bool {
+        source
+            .chars()
+            .all(|c| c == '-' || c != '_' && c.is_lowercase())
     }
     pub fn is_constant(source: &'a str) -> bool {
         source
@@ -60,20 +68,29 @@ impl<'a> NamingPrincipal<'a> {
         !source.chars().all(|c| c.is_uppercase())
     }
     pub fn is_camel(source: &'a str) -> bool {
-        if source.contains("_") {
+        if source.contains("_") || source.contains("-") {
             return false;
         }
-        let first = source.chars().next().unwrap();
-        if first.is_uppercase() {
-            return false;
+        if let Some(first) = source.chars().next() {
+            first.is_lowercase()
+        } else {
+            false
         }
-        true
     }
 }
 
 #[cfg(test)]
 mod test_naming_principal {
     use super::*;
+    #[test]
+    fn test_is_chain_and_new_chain() {
+        let source = "chain-case";
+        assert!(NamingPrincipal::is_chain(source));
+        let np = NamingPrincipal::new(source);
+        assert_eq!(np, NamingPrincipal::Chain(source));
+        let source = "snake_case";
+        assert!(!NamingPrincipal::is_chain(source));
+    }
     #[test]
     fn test_is_constant_and_new_constant() {
         let source = "CONSTANT_CASE";
