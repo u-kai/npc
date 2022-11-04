292600eb0b80665175a601dacfc32793bd9e4c53
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-04 09:38:10 +0900

    add pascal

--- src/naming_principal.rs
+++ src/naming_principal.rs
@@ -25,7 +25,23 @@ enum NamingPrincipal<'a> {
 
 impl<'a> NamingPrincipal<'a> {
     pub fn new(source: &'a str) -> Self {
-        Self::Camel(source)
+        if Self::is_camel(source) {
+            return Self::Camel(source);
+        }
+        if Self::is_pascal(source) {
+            return Self::Pascal(source);
+        }
+        Self::Flat(source)
+    }
+    pub fn is_pascal(source: &'a str) -> bool {
+        if source.contains("_") {
+            return false;
+        }
+        let first = source.chars().next().unwrap();
+        if !first.is_uppercase() {
+            return false;
+        }
+        !source.chars().all(|c| c.is_uppercase())
     }
     pub fn is_camel(source: &'a str) -> bool {
         if source.contains("_") {
@@ -42,6 +58,15 @@ impl<'a> NamingPrincipal<'a> {
 #[cfg(test)]
 mod test_naming_principal {
     use super::*;
+    #[test]
+    fn test_is_pascal_and_new_pascal() {
+        let source = "PascalCase";
+        assert!(NamingPrincipal::is_pascal(source));
+        let np = NamingPrincipal::new(source);
+        assert_eq!(np, NamingPrincipal::Pascal(source));
+        let source = "snake_case";
+        assert!(!NamingPrincipal::is_pascal(source));
+    }
     #[test]
     fn test_is_camel_and_new_camel() {
         let source = "camelCase";
